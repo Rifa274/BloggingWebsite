611e25c40f049ccf238b2b88b0d6807c7cd2b4c6
rifasanjida <rifaasaanjida@gmail.com>
2025-07-20 07:06:36 +0600

    adding login and registration page

--- bloggingweb/blogweb/urls.py
+++ bloggingweb/blogweb/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.print_hello, name='hello'),
-    path('first_html/',views.first_html,name='firsthtml')
+    path('first_html/',views.first_html,name='firsthtml'),
+    path('login/', views.login_page, name='login'),
+    path('registration/',views.registration_page,name='registration'),
+    
 ]
--- bloggingweb/blogweb/views.py
+++ bloggingweb/blogweb/views.py
@@ -7,3 +7,7 @@ def print_hello(request):
     return HttpResponse("Hello world!")
 def first_html(request):
     return render(request,"first.html")
+def login_page(request):
+    return render(request,"login.html")
+def registration_page(request):
+    return render(request,"registration.html")
